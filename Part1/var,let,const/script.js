cc47946dcf21766fe3695c12d343897c9883ba08
2-jun9 <jungu269@gmail.com>
2023-06-27 17:01:32 +0900

    ADD: test files

---
+++ Part1/var,let,const/script.js
@@ -0,0 +1,19 @@
+// var / let / contst
+var greeting = "hello";
+console.log(greeting);
+
+var greeting = "hi";
+console.log(greeting);
+
+greeting = "how are you?";
+console.log(greeting);
+// var는 더이상 사용하지 않음
+// 함수 레벨 스코프(참조 범위)
+
+// let은 중복선언은 불가능하지만 재할당은 가능함
+// 블록 레벨 스코프,
+let greema = "hello";
+console.log(greema);
+
+// const는 중복선언도 불가, 재할당도 불가
+// 블록 레벨 스코프
