55ee40815a657d37e2badea585a584f7dd474f46
2-jun9 <jungu269@gmail.com>
2023-06-28 15:58:36 +0900

    ADD: js type

---
+++ Part1/type/test.js
@@ -0,0 +1,13 @@
+let foo = 42;
+
+console.log(typeof foo);
+
+const name = "han";
+
+let anything;
+// 선언 후 할당을 하지 않으면 undefined가 할당됨
+
+const hobbies = ["walking", "boooks"];
+
+const address = {};
+// 배열도 오브젝트의 일종이므로 type으로 검사하면 Object로 나옴
