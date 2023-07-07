f20e72ed0e602d3d9661098785e37c47f58b6255
2-jun9 <jungu269@gmail.com>
2023-07-07 17:50:51 +0900

    EDIT: MVC 패턴 적용

--- Part3/express/server.js
+++ Part3/express/server.js
@@ -1,17 +1,7 @@
 const express = require("express");
-
+const usersController = require("./controllers/users.controller");
+const postsController = require("./controllers/posts.constroller");
 const PORT = 4000;
-
-const Users = [
-  {
-    id: 0,
-    name: "James",
-  },
-  {
-    id: 1,
-    name: "Chris",
-  },
-];
 
 const app = express();
 
@@ -25,40 +15,15 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url} ${diffTime}ms`);
 });
 
-app.get("/users", (req, res) => {
-  res.json(Users);
-});
-
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.post("/users", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({
-      //return을 삽입하지 않으면 밑에 까지 실행됨, 끊어주는 역할
-      error: "Missing user name",
-    });
-  }
-  // user 측에서 잘못된 값을 보냈을 경우 에러 반환
+app.get("/users", usersController.getUsers);
+app.get("/users/:userId", usersController.getUser);
+app.post("/users", usersController.postUser);
 
-  const newUser = {
-    name: req.body.name,
-    id: Users.length,
-  };
-  Users.push(newUser);
-  res.json(newUser);
-});
-
-app.get("/users/:userId", (req, res) => {
-  const userId = Number(req.params.userId);
-  const user = Users[userId];
-  if (user) {
-    res.jsonp(user);
-  } else {
-    res.sendStatus(404);
-  }
-});
+app.get(" /posts", postsController.getPost);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
