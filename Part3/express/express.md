6c268e7098c9da61841b133370ec66903443cff7
2-jun9 <jungu269@gmail.com>
2023-07-07 16:42:44 +0900

    ADD: params query

--- Part3/express/express.md
+++ Part3/express/express.md
@@ -4,4 +4,6 @@ nodejs web framework
 nodejs의 api를 쉽게 사용할 수 있게 해 줌. 사용자 수가 많아 레퍼런스가 많음
 express를 기반으로 다른 framework가 만들어지기도 하므로 알아두면 다른거 배우기도 쉬움
 
-##
+## res.send() vs res.end()
+
+데이터를 제공하지 않고 응답을 종료하려면 res.end()사용 -> 404 등에 사용
--- Part3/express/server.js
+++ Part3/express/server.js
@@ -1,9 +1,34 @@
 const express = require("express");
 
 const PORT = 4000;
-const HOST = "0.0.0.0";
+
+const User = [
+  {
+    id: 0,
+    name: "James",
+  },
+  {
+    id: 1,
+    name: "Chris",
+  },
+];
 
 const app = express();
+
+app.get("/users", (req, res) => {
+  res.json(User);
+});
+
+app.get("/users/:userId", (req, res) => {
+  const userId = Number(req.params.userId);
+  const user = User[userId];
+  if (user) {
+    res.jsonp(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
