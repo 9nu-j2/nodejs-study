f949fabe7d090c3b9a0c41e306d33b102cc6d162
2-jun9 <jungu269@gmail.com>
2023-07-07 18:51:52 +0900

    ADD: template engine

--- Part3/express/controllers/posts.constroller.js
+++ Part3/express/controllers/posts.constroller.js
@@ -1,7 +1,9 @@
 const path = require("path");
 
 function getPost(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "images", "forest.jpeg"));
+  res.render("posts", {
+    templateNmae: "posts",
+  });
 }
 
 module.exports = {
--- Part3/express/server.js
+++ Part3/express/server.js
@@ -5,8 +5,15 @@ const PORT = 4000;
 const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
 const app = express();
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views")); // 템플릿 엔진 서버 등록
 
 app.use("/static", express.static(path.join(__dirname, "public"))); // 정작 파일 웹 제공, 절대경로로 제공
+app.get("/", (req, res) => {
+  res.render("index", {
+    imageTitle: "It is a forest 2",
+  });
+});
 
 app.use(express.json()); // body parser 대체 내장 모듈
 
