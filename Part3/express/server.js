bf852278e27370e6f7c042298615f3e3a6dee48b
2-jun9 <jungu269@gmail.com>
2023-07-07 18:37:12 +0900

    ADD: 정적파일 제공하기

--- Part3/express/server.js
+++ Part3/express/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const path = require("path");
 const PORT = 4000;
 
 const usersRouter = require("./routes/users.router");
 const postsRouter = require("./routes/posts.router");
 const app = express();
 
+app.use("/static", express.static(path.join(__dirname, "public"))); // 정작 파일 웹 제공, 절대경로로 제공
+
 app.use(express.json()); // body parser 대체 내장 모듈
+
 app.use((req, res, next) => {
   const start = Date.now();
   console.log(`${req.method}, ${req.url}`);
